10c6ae222d0c5c96c28a3c80dbca6fd19384c1fd
pranjalr20 <100490494+pranjalr20@users.noreply.github.com>
2022-10-12 16:02:21 +0530

    Add files via upload

---
+++ Anagram.java
@@ -0,0 +1,45 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.spi.ToolProvider;
+
+class Anagram {
+  public static void main(String[] args) {
+
+   
+    Scanner input = new Scanner(System.in);
+
+    
+    System.out.print("Enter first String: ");
+    String str1 = input.nextLine();
+    System.out.print("Enter second String: ");
+    String str2 = input.nextLine();
+
+   
+    if(str1.length() == str2.length()) {
+
+      
+      char[] charArray1 = str1.toCharArray();
+      char[] charArray2 = str2.toCharArray();
+
+      
+      Arrays.sort(charArray1);
+      Arrays.sort(charArray2);
+
+      
+      boolean result = Arrays.equals(charArray1, charArray2);
+
+      if(result) {
+        System.out.println("True");
+      }
+      else {
+        System.out.println("False");
+      }
+    }
+    else {
+        System.out.println("False");
+      }
+    
+
+    input.close();
+  }
+}
---
+++ SumTo100.java
@@ -0,0 +1,139 @@
+import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+public class SumTo100 implements Runnable {
+
+    public static void main(String[] args) {
+        new SumTo100().run();
+    }
+
+    void print(int givenSum) {
+        Expression expression = new Expression();
+        for (int i = 0; i < Expression.NUMBER_OF_EXPRESSIONS; i++, expression.next()) {
+            if (expression.toInt() == givenSum) {
+                expression.print();
+            }
+        }
+    }
+
+    void comment(String commentString) {
+        System.out.println();
+        System.out.println(commentString);
+        System.out.println();
+    }
+
+    
+    public void run() {
+        final Stat stat = new Stat();
+
+        comment("Show all solutions that sum to 100");
+        final int givenSum = 100;
+        print(givenSum);
+
+        
+    }
+
+    private static class Expression {
+
+        private final static int NUMBER_OF_DIGITS = 9;
+        private final static byte ADD = 0;
+        private final static byte SUB = 1;
+        private final static byte JOIN = 2;
+
+        final byte[] code = new byte[NUMBER_OF_DIGITS];
+        final static int NUMBER_OF_EXPRESSIONS = 2 * 3 * 3 * 3 * 3 * 3 * 3 * 3 * 3;
+
+        Expression next() {
+            for (int i = 0; i < NUMBER_OF_DIGITS; i++) {
+                if (++code[i] > JOIN) {
+                    code[i] = ADD;
+                } else {
+                    break;
+                }
+            }
+            return this;
+        }
+
+        int toInt() {
+            int value = 0;
+            int number = 0;
+            int sign = (+1);
+            for (int digit = 1; digit <= 9; digit++) {
+                switch (code[NUMBER_OF_DIGITS - digit]) {
+                    case ADD:
+                        value += sign * number;
+                        number = digit;
+                        sign = (+1);
+                        break;
+                    case SUB:
+                        value += sign * number;
+                        number = digit;
+                        sign = (-1);
+                        break;
+                    case JOIN:
+                        number = 10 * number + digit;
+                        break;
+                }
+            }
+            return value + sign * number;
+        }
+
+        
+        public String toString() {
+            StringBuilder s = new StringBuilder(2 * NUMBER_OF_DIGITS + 1);
+            for (int digit = 1; digit <= NUMBER_OF_DIGITS; digit++) {
+                switch (code[NUMBER_OF_DIGITS - digit]) {
+                    case ADD:
+                        if (digit > 1) {
+                            s.append('+');
+                        }
+                        break;
+                    case SUB:
+                        s.append('-');
+                        break;
+                }
+                s.append(digit);
+            }
+            return s.toString();
+        }
+
+        void print() {
+            print(System.out);
+        }
+
+        void print(PrintStream printStream) {
+            printStream.format("%9d", this.toInt());
+            printStream.println(" = " + this);
+        }
+    }
+
+    private static class Stat {
+
+        final Map<Integer, Integer> countSum = new HashMap<>();
+        final Map<Integer, Set<Integer>> sumCount = new HashMap<>();
+
+        Stat() {
+            Expression expression = new Expression();
+            for (int i = 0; i < Expression.NUMBER_OF_EXPRESSIONS; i++, expression.next()) {
+                int sum = expression.toInt();
+                countSum.put(sum, countSum.getOrDefault(sum, 0) + 1);
+            }
+            for (Map.Entry<Integer, Integer> entry : countSum.entrySet()) {
+                Set<Integer> set;
+                if (sumCount.containsKey(entry.getValue())) {
+                    set = sumCount.get(entry.getValue());
+                } else {
+                    set = new HashSet<>();
+                }
+                set.add(entry.getKey());
+                sumCount.put(entry.getValue(), set);
+            }
+        }
+    }
+}
